a61c4ea72abd03a482387937a861f6afe0c7e551
Nicole Moyo <n.moyo@alustudent.com>
2024-01-16 17:57:46 +0200

    get employee data through api

--- client/src/tabs/employees/index.js
+++ client/src/tabs/employees/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import Header from '../../components/Header';
 import { DataGrid } from '@mui/x-data-grid';
+import Axios from 'axios';
 
 const Employees = () => {
-  return (
-    <div>Employees</div>
-  )
+
+    const [data, setData] = useState();
+
+    const getData = useCallback(() => {
+        Axios.get(`http://localhost:5000/api/v1/admin/employees`).then(response => {
+            if (response.status !== 200) return;
+            console.log(response.data)
+            setData(response.data);
+
+        })
+        ;
+    }, []);
+
+    useEffect(() => {
+        getData();
+    }, [getData])
+
+    return (
+        <div>Employees {JSON.stringify(data)}</div>
+    )
 }
 
 export default Employees;
